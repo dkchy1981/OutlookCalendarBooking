1f59b2c4cbfa2d17a2fb61edb60511abc41042e3
dc <dc@BRDNOODLES>
2018-05-30 11:26:07 +0530

    Updated code for login functionality

--- AppointmentBooking/AppointmentBooking/Controllers/HomeController.cs
+++ AppointmentBooking/AppointmentBooking/Controllers/HomeController.cs
@@ -9,6 +9,8 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Net;
 using System.DirectoryServices;
+using System.Web.Security;
+using System.Security.Principal;
 
 namespace AppointmentBooking.Controllers
 {
@@ -16,7 +18,60 @@ namespace AppointmentBooking.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }       
+            if (Request.IsAuthenticated)
+            {
+                return View();
+            }
+            else
+            {
+                return Logout();
+            }
+        }
+
+
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            try
+            {
+                // First we clean the authentication ticket like always  
+                //required NameSpace: using System.Web.Security;  
+                FormsAuthentication.SignOut();
+
+                // Second we clear the principal to ensure the user does not retain any authentication  
+                //required NameSpace: using System.Security.Principal;  
+                HttpContext.User = new GenericPrincipal(new GenericIdentity(string.Empty), null);
+
+                Session.Clear();
+                System.Web.HttpContext.Current.Session.RemoveAll();
+
+                // Last we redirect to a controller/action that requires authentication to ensure a redirect takes place  
+                // this clears the Request.IsAuthenticated flag since this triggers a new request  
+                return RedirectToLocal();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+
+        private ActionResult RedirectToLocal(string returnURL = "")
+        {
+            try
+            {
+                // If the return url starts with a slash "/" we assume it belongs to our site  
+                // so we will redirect to this "action"  
+                if (!string.IsNullOrWhiteSpace(returnURL) && Url.IsLocalUrl(returnURL))
+                    return Redirect(returnURL);
+
+                // If we cannot verify if the url is local to our host we redirect to a default location  
+                return RedirectToAction("Login", "Login");
+            }
+            catch
+            {
+                throw;
+            }
+        }
     }
 }
--- AppointmentBooking/AppointmentBooking/Controllers/LoginController.cs
+++ AppointmentBooking/AppointmentBooking/Controllers/LoginController.cs
@@ -3,6 +3,8 @@ using System.Web.Mvc;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web.Security;
+using System.Security.Principal;
 
 namespace AppointmentBooking.Controllers
 {
@@ -35,6 +37,8 @@ namespace AppointmentBooking.Controllers
                     if (response.Result.StatusCode == System.Net.HttpStatusCode.OK)
                     {
                         Session["UserName"] = userInfo.userName;
+                        FormsAuthentication.SignOut();
+                        FormsAuthentication.SetAuthCookie(userInfo.userName, true);
                         return Redirect("~/Home/Index");
                     }
                     else
@@ -47,6 +51,51 @@ namespace AppointmentBooking.Controllers
             catch
             {
                 return View("Login");
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Logout()
+        {
+            try
+            {
+                // First we clean the authentication ticket like always  
+                //required NameSpace: using System.Web.Security;  
+                FormsAuthentication.SignOut();
+
+                // Second we clear the principal to ensure the user does not retain any authentication  
+                //required NameSpace: using System.Security.Principal;  
+                HttpContext.User = new GenericPrincipal(new GenericIdentity(string.Empty), null);
+
+                Session.Clear();
+                System.Web.HttpContext.Current.Session.RemoveAll();
+
+                // Last we redirect to a controller/action that requires authentication to ensure a redirect takes place  
+                // this clears the Request.IsAuthenticated flag since this triggers a new request  
+                return RedirectToLocal();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private ActionResult RedirectToLocal(string returnURL = "")
+        {
+            try
+            {
+                // If the return url starts with a slash "/" we assume it belongs to our site  
+                // so we will redirect to this "action"  
+                if (!string.IsNullOrWhiteSpace(returnURL) && Url.IsLocalUrl(returnURL))
+                    return Redirect(returnURL);
+
+                // If we cannot verify if the url is local to our host we redirect to a default location  
+                return RedirectToAction("Login", "Login");
+            }
+            catch
+            {
+                throw;
             }
         }
     }

4cff801df1558f1543eb7182a58c669c0a3cb98b
Rachit Chauhan <RachitC@sfwltd.co.uk>
2018-06-13 14:51:15 +0530

    Custom CheckBox in All Browsers

--- AppointmentBooking/AppointmentBooking/Content/JavaScripts/UIScripts.js
+++ AppointmentBooking/AppointmentBooking/Content/JavaScripts/UIScripts.js
@@ -140,8 +140,22 @@ function customRadio(radioName) {
     });
 }
 
+function customCheckbox(checkboxName) {
+    var checkBox = $('input[name="' + checkboxName + '"]');
+    $(checkBox).each(function () {
+        $(this).wrap("<span class='custom-checkbox'></span>");
+        if ($(this).is(':checked')) {
+            $(this).parent().addClass("selected");
+        }
+    });
+    $(checkBox).click(function () {
+        $(this).parent().toggleClass("selected");
+    });
+}
+
 
 $(document).ready(function () {
     customRadio("DailySelectionType");
     customRadio("MonthlySelectionType");
+    customCheckbox("WeeklySelectionType");
 })
